1dc60284afac4498caed6b4d382daf34d01ae5f8
Mostafa Bkry <mbkry661@gmail.com>
2024-08-31 02:09:41 +0300

    finished app design

---
+++ Dialogs/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Dialogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnOpen = new Button();
+            btnSave = new Button();
+            btnExit = new Button();
+            btnFont = new Button();
+            btnColor = new Button();
+            btnMyDlg = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            btnOpen.Location = new Point(12, 12);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(75, 23);
+            btnOpen.TabIndex = 0;
+            btnOpen.Text = "Open";
+            btnOpen.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            btnSave.Anchor = AnchorStyles.Top;
+            btnSave.Location = new Point(234, 12);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 1;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnExit.Location = new Point(456, 12);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 2;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnFont
+            // 
+            btnFont.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnFont.Location = new Point(12, 318);
+            btnFont.Name = "btnFont";
+            btnFont.Size = new Size(75, 23);
+            btnFont.TabIndex = 3;
+            btnFont.Text = "Font";
+            btnFont.UseVisualStyleBackColor = true;
+            // 
+            // btnColor
+            // 
+            btnColor.Anchor = AnchorStyles.Bottom;
+            btnColor.Location = new Point(234, 319);
+            btnColor.Name = "btnColor";
+            btnColor.Size = new Size(75, 23);
+            btnColor.TabIndex = 4;
+            btnColor.Text = "Color";
+            btnColor.UseVisualStyleBackColor = true;
+            // 
+            // btnMyDlg
+            // 
+            btnMyDlg.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnMyDlg.Location = new Point(456, 319);
+            btnMyDlg.Name = "btnMyDlg";
+            btnMyDlg.Size = new Size(75, 23);
+            btnMyDlg.TabIndex = 5;
+            btnMyDlg.Text = "MyDialog";
+            btnMyDlg.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(12, 60);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(519, 233);
+            richTextBox1.TabIndex = 6;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(544, 354);
+            Controls.Add(richTextBox1);
+            Controls.Add(btnMyDlg);
+            Controls.Add(btnColor);
+            Controls.Add(btnFont);
+            Controls.Add(btnExit);
+            Controls.Add(btnSave);
+            Controls.Add(btnOpen);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Text Editor";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnOpen;
+        private Button btnSave;
+        private Button btnExit;
+        private Button btnFont;
+        private Button btnColor;
+        private Button btnMyDlg;
+        private RichTextBox richTextBox1;
+    }
+}
